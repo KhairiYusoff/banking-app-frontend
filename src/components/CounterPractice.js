14a178e789bc26c31d781a36bf2176e012652cca
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-12-27 22:32:21 +0800

    update

--- src/components/CounterPractice.js
+++ src/components/CounterPractice.js
@@ -1,14 +1,23 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { counterPracticeActions } from "../store/counterPractice"
 
 const CounterPractice = () => {
 
     const counter = useSelector(state => state.counterPractice.count)
+    const dispatch = useDispatch()
     console.log(counter)
 
+    const myStyle = {
+        color: 'white'
+    }
+
     return (
-        <div>
+        <div style={myStyle}>
             <h1>Counter Practice</h1>
             <h2>{counter}</h2>
+            <button onClick={() => dispatch(counterPracticeActions.increment())}>Increment</button>
+            <button onClick={() => dispatch(counterPracticeActions.decrement())}>Decrement</button>
+            <button onClick={() => dispatch(counterPracticeActions.reset())}>reset</button>
         </div>
     )
 }
